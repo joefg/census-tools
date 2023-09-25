cd5ce1d12baded4fcb89bb954f23f4fd4e83afd6
JFG <joefullergray@gmail.com>
2023-09-25 20:47:20 +0100

    data: Add spatial index to constituency-22

--- data/importers/constituencies-2022.sh
+++ data/importers/constituencies-2022.sh
@@ -11,9 +11,10 @@ function add_pcons(){
 	unzip "downloads/Westminster_Parliamentary_Constituencies_Dec_2022_UK_BGC.zip" -d build/pcon-22
 
 	local converted=$(reproject_shp "build/pcon-22/PCON_DEC_2022_UK_BGC.shp" "EPSG:4326")
-
 	spatialite_tool -i -shp "${converted}" -d "$database" \
 		-t "pcon_22" -g geometry --charset utf-8 --srid 4326
+
+	add_indexes "$database" "pcon_22" | spatialite $database
 }
 
 add_pcons $1
