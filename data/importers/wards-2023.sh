b82ca200d59ab4360a452b5a8671d9abf2ad8dac
JFG <joefullergray@gmail.com>
2023-10-12 19:27:06 +0100

    data: Add spatial index to wards-2023

--- data/importers/wards-2023.sh
+++ data/importers/wards-2023.sh
@@ -3,16 +3,18 @@
 source importers/utils.sh
 
 function add_wards(){
-	local database=$1
-	shift
+  local database=$1
+  shift
 
-	rm -r build/wards-23
-	mkdir -p build/wards-23
-	unzip "downloads/Wards_(May_2023)_Boundaries_UK_BGC.zip" -d build/wards-23 
+  rm -r build/wards-23
+  mkdir -p build/wards-23
+  unzip "downloads/Wards_(May_2023)_Boundaries_UK_BGC.zip" -d build/wards-23 
 
-	local converted=$(reproject_shp "build/wards-23/WD_MAY_2023_UK_BGC.shp" "EPSG:4326")
-	spatialite_tool -i -shp "${converted}" -d "$database" \
-		-t "wards_23" -g geometry -c utf-8 -s 4326
+  local converted=$(reproject_shp "build/wards-23/WD_MAY_2023_UK_BGC.shp" "EPSG:4326")
+  spatialite_tool -i -shp "${converted}" -d "$database" \
+    -t "wards_23" -g geometry -c utf-8 -s 4326
+
+  add_indexes "$database" "wards_23" | spatialite $database
 }
 
 add_wards $1
