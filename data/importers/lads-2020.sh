dcbacf3592b484d8955f3c77385848ca5180181d
JFG <joefullergray@gmail.com>
2023-10-12 19:21:41 +0100

    data: Add spatial index to lads-2020

--- data/importers/lads-2020.sh
+++ data/importers/lads-2020.sh
@@ -13,6 +13,8 @@ function add_lads(){
 	local converted=$(reproject_shp "build/lad-20/LAD_DEC_2020_UK_BGC.shp" "EPSG:4326")
 	spatialite_tool -i -shp "${converted}" -d "$database" \
 		-t "lad_20" -g geometry -c utf-8 -s 4326
+
+	add_indexes "$database" "lad_20" | spatialite $database
 }
 
 add_lads $1
