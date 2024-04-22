1e339eb717111b52d141f00e29e08f328f0ee44f
joefg <joefullergray@gmail.com>
2024-04-22 23:02:00 +0100

    feature: Enable DictCursor for Spatialite queries
    The DictCursor-like functionality enables a result to be interpolated
    like a list of dictionarirs, although this does come at the expense of
    performance on larger queries.

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,11 +3,14 @@ import census_tools.db as db
 
 def test_SpatialiteConnection():
     with db.SpatialiteConnection() as in_memory:
-        cur = in_memory.execute('select 1;')
-        assert cur.fetchall() == [(1,)]
+        cur = in_memory.execute('select 1 as "test";')
+        ret = cur.fetchall()
+        assert ret == [{'test': 1}]
 
     with db.SpatialiteConnection('/tmp/test.db') as in_file:
-        cur = in_file.execute('select 2;')
-        assert cur.fetchall() == [(2,)]
+        cur = in_file.execute('select 2 as "test";')
+        ret = cur.fetchall()
+        assert ret == [{'test': 2}]
 
+    assert os.path.isfile('/tmp/test.db')
     os.remove('/tmp/test.db')
